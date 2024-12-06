afe4dd76ee04af169e1286e9570b80a4f68681fe
SemenBerezovskiy <arsikqa43@gmail.com>
2024-12-06 18:10:01 +0700

    Nahui levakov

--- Taxer/taxes.c
+++ Taxer/taxes.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Factory{
+	int Customers;
+	int Income;
+	char Name[20];
+	char isCountryF;
+}Fac;
 unsigned int taxer(int Money, int timeper)
 {
 	int Res = Money*timeper*0.01;
@@ -17,8 +24,16 @@ unsigned int taxer(int Money, int timeper)
 	}
 
 }
+int HowIsMyFactory(Fac *Factory)
+{	
+	printf("%d\n", Factory->Customers);
+	system("clear");
+	system("wait");
+}
 int main(void)
 {
 	int n = taxer(10000, 22);
+	Fac PAZ = {1000, 10000000, "Pazovoe", 't'};
+	HowIsMyFactory(&PAZ);
 	printf("%d", n);
 }
